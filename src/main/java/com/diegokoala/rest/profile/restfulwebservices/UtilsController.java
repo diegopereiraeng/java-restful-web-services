3bd542fcd33fbf2f4a05183db6fc08d8f4240c13
Diego Koala Pereira <diegopereiraeng@gmail.com>
2021-01-17 19:57:05 -0300

    v0.3

--- src/main/java/com/diegokoala/rest/profile/restfulwebservices/UtilsController.java
+++ src/main/java/com/diegokoala/rest/profile/restfulwebservices/UtilsController.java
@@ -1,5 +1,7 @@
 package com.diegokoala.rest.profile.restfulwebservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,9 @@ import java.net.UnknownHostException;
 
 @RestController
 public class UtilsController {
+
+    @Autowired
+    public BuildProperties buildProperties;
 
     @GetMapping(path = "/api/v1/hostname")
     public static String getHostName() {
@@ -20,8 +25,17 @@ public class UtilsController {
         }
         return hostName;
     }
-
-    public static void main(String[] args) {
-        System.out.println(getHostName());
+    @GetMapping("/build-info")
+    public BuildProperties buildInfo() {
+        return this.buildProperties;
     }
+
+    /*
+    @GetMapping("/version")
+    public String version() {
+        return this.buildProperties.getVersion();
+    }
+    */
+
+
 }
