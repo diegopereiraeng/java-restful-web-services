c82e1c59a83603b6d4b22057182de4763771336c
Diego Koala Pereira <diegopereiraeng@gmail.com>
2021-02-01 12:20:01 -0300

    Appdynamics

--- src/main/java/com/diegokoala/rest/profile/restfulwebservices/ResumeController.java
+++ src/main/java/com/diegokoala/rest/profile/restfulwebservices/ResumeController.java
@@ -4,12 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class ResumeController
 {
 
     @RequestMapping(method = RequestMethod.GET, path = "/api/v1/resume")
     public String GetResume(){
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
         return "{ nome: 'Diego Paes Ramalho Pereira', position: 'Senior Sales Engineer', company: 'Cisco - Appdynamics', objective:''  }";
 
     }
