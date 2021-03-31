495de21173e79288aa7e2029d9b88db44ea98f95
Diego Paes Ramalho Pereira <koaladiego@Diego-Pereira-MBP.local>
2021-03-31 10:47:47 -0400

    drone

--- src/main/java/com/diegokoala/rest/profile/restfulwebservices/ResumeController.java
+++ src/main/java/com/diegokoala/rest/profile/restfulwebservices/ResumeController.java
@@ -14,11 +14,22 @@ public class ResumeController
     @RequestMapping(method = RequestMethod.GET, path = "/api/v1/resume")
     public String GetResume(){
         try {
-            TimeUnit.SECONDS.sleep(4);
+            TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException ie) {
             Thread.currentThread().interrupt();
         }
-        return "{ nome: 'Diego Paes Ramalho Pereira', position: 'Senior Sales Engineer', company: 'Cisco - Appdynamics', objective:''  }";
+        return "{ nome: 'Diego Paes Ramalho Pereira', position: 'Senior Sales Engineer', company: 'Harness', objective:'Help Customers with CI/CD'  }";
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/api/v1/harness")
+    public String GetHarness(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+        return "{ Message: 'Welcome to Harness, Your Deployment 1.15 was successful implemented', status: 'UP' }";
 
     }
 
