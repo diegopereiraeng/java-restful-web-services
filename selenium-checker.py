aa4dc557ae304356c5f1fb8e1d61658529fa848b
Diego Paes Ramalho Pereira <koaladiego@Diego-Pereira-MBP.local>
2021-04-05 18:46:22 -0400

    Selenium

--- selenium-checker.py
+++ selenium-checker.py
@@ -9,12 +9,21 @@ d['loggingPrefs'] = {'performance': 'ALL'}
 
 url = sys.argv[1]
 
+print("Starting Selenium Test for URL: "+url)
+
 #  ChromeOptions
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
-print(driver.title)
+""" print(driver.title)
 
-print(driver.page_source)
+print(driver.page_source) """
+for request in driver.requests:
+    if request.response:
+        print(
+            request.url,
+            request.response.status_code,
+            request.response.headers['Content-Type']
+        )
